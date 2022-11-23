ca5316da1114e9d00b9d9662eb27777faaf19b79
ehdgml9814 <113023345+ehdgml9814@users.noreply.github.com>
2022-11-23 12:34:34 +0900

    Add files via upload

---
+++ math.h
@@ -0,0 +1,13 @@
+#include <math.h>
+double stand(int* num)
+{
+	double sum = 0;
+	int i;
+	for (i = 0; i < 5; i++)
+		sum += num[i];
+	double aver = sum / 5;
+	sum = 0;
+	for (i = 0; i < 5; i++)
+		sum += pow(num[i] - aver, 2);
+	double sig = sqrt(sum / 5);
+}
